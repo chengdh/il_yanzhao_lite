7e350493e8a684ddeda7a21aeaa2526787237845
chengdh <cheng.donghui@gmail.com>
2011-08-01 18:25:42 +0800

    fixed some bug

--- app/models/computer_bill.rb
+++ app/models/computer_bill.rb
@@ -2,7 +2,7 @@
 #机打票
 class ComputerBill < CarryingBill
   #创建/修改数据前生成票据编号和货号
-  validates_presence_of :to_org_id
+  validates_presence_of :to_org_id,:bill_no,:goods_no
   #计算手续费
   before_save :cal_hand_fee
 end
--- app/models/kids_transit_bill.rb
+++ app/models/kids_transit_bill.rb
@@ -1,2 +1,3 @@
 class KidsTransitBill < CarryingBill
+  validates :transit_org_id,:to_area,:bill_no,:goods_no,:presence => true
 end
