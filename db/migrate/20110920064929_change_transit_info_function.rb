3a75d9187e12ebfa929f520bd449bd7b52f3d953
chengdh <cheng.donghui@gmail.com>
2011-09-20 15:02:25 +0800

    change carrying bill reset function

---
+++ db/migrate/20110920064929_change_transit_info_function.rb
@@ -0,0 +1,27 @@
+class ChangeTransitInfoFunction < ActiveRecord::Migration
+  def self.up
+    #添加货物中转的update功能
+    sf = SystemFunction.find_by_subject_title("货物中转")
+    func_obj =  {
+      :subject => "TransitInfo",
+      :action => :update
+    }
+    if sf.present?
+      ops = sf.system_function_operates.find_or_create_by_name("修改")
+      ops.update_attributes(:function_obj => func_obj)
+    end
+    #运单重置功能
+    sf = SystemFunction.find_by_subject_title("运单修改")
+    func_obj =  {
+      :subject => "CarryingBill",
+      :action => :reset
+    }
+    if sf.present?
+      ops = sf.system_function_operates.find_or_create_by_name("重置运单")
+      ops.update_attributes(:function_obj => func_obj)
+    end
+  end
+
+  def self.down
+  end
+end
