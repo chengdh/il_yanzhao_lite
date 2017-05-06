1a9740a5c3c1ce726f764284852e7505df24ebbe
chengdh <cheng.dongh@gmail.com>
2017-05-07 00:50:19 +0800

    change sessions_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,8 @@
 #coding: utf-8
 module SessionsHelper
   def user_roles_for_select(user)
-    ret = user.user_roles.search(:role_is_active_eq => true,:is_select_eq => true).all.map {|ur| [ur.role.name,ur.role.id]} if user.present?
+    # ret = user.user_roles.search(:role_is_active_eq => true,:is_select_eq => true).all.map {|ur| [ur.role.name,ur.role.id]} if user.present?
+    ret = user.roles.where(:is_active => true).all.map {|r| [r.name,r.id]} if user.present?
     ret = Role.where(:is_active => true).all.map {|r| [r.name,r.id]} if user.is_admin?
     ret
   end
