e9e9db10df82b03b544dfc728852b0dd9c098c0b
chengdh <cheng.dongh@gmail.com>
2017-08-13 09:36:48 +0800

    修改了新建用户错误的问题

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 #coding: utf-8
 class UsersController < BaseController
   table :username,:real_name,:is_active,:is_admin,:default_org_id,:default_role_id,:current_sign_in_at,:last_sign_in_at,:current_sign_in_ip,:last_sign_in_ip
-  http_cache :new,:last_modified => Proc.new {|c| c.send(:last_modified)},:etag => Proc.new {|c| c.send(:etag,"user_new")}
+  # http_cache :new,:last_modified => Proc.new {|c| c.send(:last_modified)},:etag => Proc.new {|c| c.send(:etag,"user_new")}
 
   def new
     get_resource_ivar || set_resource_ivar(User.new_with_roles)
