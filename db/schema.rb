71d97e25f73bde1d1070d69b9f755ae07c3ec1c7
chengdh <cheng.donghui@gmail.com>
2018-04-23 11:33:48 +0800

    修改了运单录入-客户编号限制所属机构的问题

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20180313032844) do
+ActiveRecord::Schema.define(:version => 20180423031604) do
 
   create_table "banks", :force => true do |t|
     t.string   "name",                                       :null => false
